eaa4db2c3aca7a23e60b800211aa6b27ab92dd36
Louis_Ekpenga <ekpenga9000@gmail.com>
2024-08-30 11:21:37 -0600

    Add API Key to the environment variables and make api call.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
 import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
+
 function App() {
-  return <div className="bg-green-500">Weather App</div>;
+  const API_KEY = import.meta.env.VITE_API_KEY;
+
+  const fetchWeatherData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${`calgary`}&units=metric&exclude=alerts,minutely&appid=${API_KEY}`
+      );
+      
+      console.log(response.data);
+    } catch (error) {
+      console.log(error); 
+    }
+  };
+
+  useEffect(() => {
+    fetchWeatherData();
+  }, []);
+
+
+  return (
+    <div>
+      sOMETHING
+    </div>
+  )
 }
 
 export default App;
