68c9ea19a8ca6ed3717fd0ed39674472131637dd
Louis_Ekpenga <ekpenga9000@gmail.com>
2024-08-31 09:21:52 -0600

    Add a custom icon

--- src/components/DashboardMap.tsx
+++ src/components/DashboardMap.tsx
@@ -1,5 +1,6 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'; // Import Popup from react-leaflet
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'; 
 import { useAppSelector } from '../hooks';
+import { Icon } from 'leaflet';
 import "leaflet/dist/leaflet.css";
 
 const DashboardMap = () => {
@@ -8,7 +9,12 @@ const DashboardMap = () => {
   // Ensure that latitude and longitude are both numbers before creating the position array
   const position: [number, number] | undefined = 
     data?.city.coord.lat !== undefined && data?.city.coord.lon !== undefined ? [data?.city.coord.lat, data?.city.coord.lon] : undefined;
-    
+
+  //creating a custom Icon
+  const customIcon = new Icon({
+    iconUrl: "./pin.png", 
+    iconSize: [38, 38],
+  })
   return (
     <MapContainer center={position || [51.049999, -114.066666]} zoom={10} style={{ height: '15rem', width: '100%', borderRadius:"1.5rem" }}>
       <TileLayer
@@ -16,7 +22,7 @@ const DashboardMap = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {position && (
-        <Marker position={position}>
+        <Marker position={position} icon={customIcon}>
           <Popup>{`City: ${city}`}</Popup> 
         </Marker>
       )}
